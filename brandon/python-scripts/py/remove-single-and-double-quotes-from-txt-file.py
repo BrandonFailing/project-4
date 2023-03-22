f8aed83d2318d22dee069551df4ecfa49d38dadd
BrandonFailing <112912121+BrandonFailing@users.noreply.github.com>
2023-03-21 19:28:03 -0500

    Final Submission

--- brandon/python-scripts/py/remove-single-and-double-quotes-from-txt-file.py
+++
@@ -1,7 +0,0 @@
-# Read the data from the input file and remove quotes
-with open('movie-list.txt', 'r') as infile:
-    data = infile.read().replace("'", "").replace('"', '')
-
-# Write the updated data to a new file
-with open('movie-list-r1.txt', 'w') as outfile:
-    outfile.write(data)
